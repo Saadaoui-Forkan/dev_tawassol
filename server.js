54b0d5a5df07956c1db6cf1bb2cec1f58e1a9bbb
unknown <forkan.saadaoui@gmail.com>
2023-08-07 22:26:29 +0100

    Route Files with express Router

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req,res) => res.send('API running'))
 
+// Define Routes
+app.get('/api/users', require('./routes/api/users'))
+app.get('/api/auth', require('./routes/api/auth'))
+app.get('/api/profile', require('./routes/api/profile'))
+app.get('/api/posts', require('./routes/api/posts'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
