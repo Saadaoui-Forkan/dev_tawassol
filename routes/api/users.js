bfb1fd5976f988b6761c36408afa46fc8bc02f53
unknown <forkan.saadaoui@gmail.com>
2023-08-10 05:55:38 +0100

    Register new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
+const User = require('../../models/User')
 
 // @route   Post api/users
 // @desc    Test route
@@ -16,13 +19,48 @@ router.post('/',
             .isLength({min: 6}),
 
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()})
         }
-        res.send('user route')
+
+        const {name, email, password} = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({email})
+            if (user) {
+                return res.status(400).json({errors: [{msg: 'User already exists'}]})
+            }
+
+            // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            // Encrypt Password
+            const salt = await bcrypt.genSalt(10)
+            user.passwod = await bcrypt.hash(password, salt)
+            await user.save()
+
+            // Return jwt
+
+            res.send('User registered')    
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).send('Server error')
+        }
+
     }
 )
 
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/', (req,res) => res.send('API running'))
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'))
-app.get('/api/auth', require('./routes/api/auth'))
-app.get('/api/profile', require('./routes/api/profile'))
-app.get('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
