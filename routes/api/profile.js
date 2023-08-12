cd1d0a458c12d5cab55e2bc5c367f2756a41889f
unknown <forkan.saadaoui@gmail.com>
2023-08-12 11:10:06 +0100

    Get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const protect = require('../../middleware/authMiddleware')
 
-// @route   Get api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('profile route'))
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
+
+// @route   Get api/profile/me
+// @desc    Get current users profile 
+// @access  Private
+router.get('/me',protect ,async(req, res) => {
+    try {
+       const profile = await Profile.findOne({ user: req.user.id })
+        .populate('user', ['name', 'avatar']) 
+
+        if (!profile) {
+            res.status(400).json({ msg: 'There is no profile for this user' })
+        }
+
+        res.json(profile)
+        
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router;
