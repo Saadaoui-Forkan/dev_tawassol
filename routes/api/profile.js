310b30fdaf5a4ac884762a329ff649eb0aa953d6
unknown <forkan.saadaoui@gmail.com>
2023-08-13 11:13:00 +0100

    Get Profile By UserId

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,4 +114,29 @@ router.get('/', async(req,res) => {
     }
 })
 
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user id
+// @access Public
+router.get('/user/:user_id', async(req,res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id})
+                                        .populate('user', ['name', 'avatar'])
+
+        if (!profile) {
+            return res.status(400).json({msg: 'Profile Not Found'})
+        }
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({msg: 'Profile Not Found'})
+            // if we enter invalid userId like 1 or 123 or ... 
+            // res will send 'Server Error'
+            // So we use err.kind that means that the profile is not found
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
