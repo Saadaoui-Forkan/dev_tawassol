13b58baeb729152dbe6eef1dede3a0e4840ae129
unknown <forkan.saadaoui@gmail.com>
2023-08-13 11:36:55 +0100

    Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,22 @@ router.get('/user/:user_id', async(req,res) => {
     }
 })
 
+// @route  DELETE api/profile
+// @desc   Delete Profile, User & Post
+// @access Private
+router.delete('/', protect, async(req,res) => {
+    try {
+        // Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id })
+
+        // Remove User
+        await User.findOneAndRemove({ _id: req.user.id })
+
+        res.json({ msg: 'user deleted' })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
