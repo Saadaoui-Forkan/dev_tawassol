f67a0e022940a2b887d8b57ef5102442875965dc
unknown <forkan.saadaoui@gmail.com>
2023-08-14 22:32:37 +0100

    Delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -191,4 +191,39 @@ router.post('/comment/:id', [protect, [
     }
 })
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', protect, async(req,res) => {
+  try {
+    
+    const post = await Post.findById(req.params.id)
+
+    // Pull out comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+    // Make sure that comment exist
+    if (!comment) {
+      res.status(404).json({msg: 'Comment does not exist'})
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      res.status(401).json({msg: 'User not authorized'})
+    }
+
+    // Get Remove Index
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id)
+    post.comments.splice(removeIndex, 1)
+
+    await post.save()
+    res.json(post.comments)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+})
+
 module.exports = router;
