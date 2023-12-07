d83ca8ded0885afabfba6eb789ae02a61d24fcce
unknown <forkan.saadaoui@gmail.com>
2023-12-07 21:54:37 +0100

    implementing redux toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
@@ -6,14 +6,14 @@ import Register from "./components/auth/Register";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar/>
       <Routes>
         <Route path="/" element={ <Landing/> }/>
         <Route path="/login" element={ <Login/> }/>
         <Route path="/register" element={ <Register/> }/>  
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
